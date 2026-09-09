37928756af13ba558107e75e39636d10c6db028d
David Stafa <david.stafa@gmail.com>
2026-09-09 16:46:45 +0200

    fix(StockCheckCheckbox): format

--- apps/web/src/domains/equipment/_shared/components/StockCheckCheckbox.tsx
+++ apps/web/src/domains/equipment/_shared/components/StockCheckCheckbox.tsx
@@ -32,9 +32,7 @@ export const StockCheckCheckbox = ({
 				className="h-5 w-5"
 			/>
 			{lastCheckedAt && (
-				<div className="text-xs text-muted-foreground">
-					{format(lastCheckedAt, 'H:mm, d.M.yy')}
-				</div>
+				<div className="text-xs text-muted-foreground">{format(lastCheckedAt, 'H:mm, d.M.yy')}</div>
 			)}
 		</div>
 	)
